8251970dfab35d2c2ea8ccdc4a7bb33eacd0452f
omijn <13493921+omijn@users.noreply.github.com>
2024-02-19 15:49:50 -0800

    add home to setup script

--- .setup-cfg.sh
+++ .setup-cfg.sh
@@ -2,13 +2,13 @@ git clone --bare https://github.com/omijn/dotfiles.git $HOME/.cfg
 function config() {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
-mkdir -p .config-backup
+mkdir -p $HOME/.config-backup
 config checkout
 if [ $? = 0 ]; then
 	echo "Checked out config.";
 else
     echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $HOME/.config-backup/{}
 fi;
 config checkout
 config config status.showUntrackedFiles no
